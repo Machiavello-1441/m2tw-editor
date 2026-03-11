5ab827ea02f2046bc887dead99d55c46d8a70280
Machiavello <machiavello1441@gmail.com>
2026-03-11 19:38:42 +0000

    File changes

--- src/pages/EDBEditor.jsx
+++ src/pages/EDBEditor.jsx
@@ -1,8 +1,8 @@
 import React, { useCallback } from 'react';
 import { useEDB } from '../components/edb/EDBContext';
 import BuildingTree from '../components/edb/BuildingTree';
-import LevelEditor from '../components/edb/LevelEditor';
-import RefFileLoader from '../components/edb/RefFileLoader';
+import LevelEditor from '../components/edb/LevelEditor.jsx';
+import RefFileLoader from '../components/edb/RefFileLoader.jsx';
 import { Button } from '@/components/ui/button';
 import { Upload, FileText, AlertCircle } from 'lucide-react';
 
