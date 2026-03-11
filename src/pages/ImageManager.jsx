64d0ef36ac4d264426514cc853812e089a7ff625
Machiavello <machiavello1441@gmail.com>
2026-03-11 19:01:30 +0000

    File changes

--- src/pages/ImageManager.jsx
+++ src/pages/ImageManager.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useEDB } from '../components/edb/EDBContext';
-import { CULTURES } from '../components/edb/EDBParser';
+import { useRefData } from '../components/edb/RefDataContext';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
