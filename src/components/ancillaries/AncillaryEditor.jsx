a499c7ab9e3eb0753cc35c0193a4353b1c8fc49c
Machiavello <machiavello1441@gmail.com>
2026-03-13 21:13:29 +0000

    File changes

--- src/components/ancillaries/AncillaryEditor.jsx
+++ src/components/ancillaries/AncillaryEditor.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useAncillaries } from './AncillariesContext';
-
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
--- src/components/traits/TraitEditor.jsx
+++ src/components/traits/TraitEditor.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useTraits } from './TraitsContext';
-
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
