2a1f1ccab853a68d7f3320abff677a69566f73bc
Machiavello <machiavello1441@gmail.com>
2026-03-13 19:06:22 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -212,12 +212,16 @@ export default function Home() {
           </label>
 
           <div className="grid grid-cols-2 sm:grid-cols-3 gap-2">
-            <FileStatus label="Buildings (EDB)" hint="export_descr_buildings.txt"    status={fileStatus.edb} />
-            <FileStatus label="Building Text"   hint="text\export_buildings.txt"     status={fileStatus.txt} />
-            <FileStatus label="Factions"        hint="descr_sm_factions.txt"         status={fileStatus.fac} />
-            <FileStatus label="Resources"       hint="descr_sm_resources.txt"        status={fileStatus.res} />
-            <FileStatus label="Units"           hint="export_descr_unit.txt"         status={fileStatus.unit} />
-            <FileStatus label="Events"          hint="descr_events.txt"              status={fileStatus.ev} />
+            <FileStatus label="Buildings (EDB)" hint="export_descr_buildings.txt"        status={fileStatus.edb} />
+            <FileStatus label="Building Text"   hint="text\export_buildings.txt"         status={fileStatus.txt} />
+            <FileStatus label="Factions"        hint="descr_sm_factions.txt"             status={fileStatus.fac} />
+            <FileStatus label="Resources"       hint="descr_sm_resources.txt"            status={fileStatus.res} />
+            <FileStatus label="Units"           hint="export_descr_unit.txt"             status={fileStatus.unit} />
+            <FileStatus label="Events"          hint="descr_events.txt"                  status={fileStatus.ev} />
+            <FileStatus label="Traits"          hint="export_descr_character_traits.txt" status={fileStatus.traits} />
+            <FileStatus label="Traits Text"     hint="text\export_VnVs.txt"              status={fileStatus.vnvs} />
+            <FileStatus label="Ancillaries"     hint="export_descr_ancillaries.txt"      status={fileStatus.anc} />
+            <FileStatus label="Ancillaries Text" hint="text\export_ancillaries.txt"      status={fileStatus.anctxt} />
           </div>
         </div>
       </div>
