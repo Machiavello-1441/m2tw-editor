c9496bd634aeffe954b17365ecaf02fe5baeea91
Machiavello <machiavello1441@gmail.com>
2026-03-13 19:05:12 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,6 +64,7 @@ export default function Home() {
 
   const [fileStatus, setFileStatus] = useState({
     edb: 'idle', fac: 'idle', res: 'idle', ev: 'idle', unit: 'idle', txt: 'idle',
+    traits: 'idle', anc: 'idle', vnvs: 'idle', anctxt: 'idle',
     map_heights: 'idle', map_ground_types: 'idle', map_climates: 'idle',
     map_regions: 'idle', map_features: 'idle', map_fog: 'idle',
   });
