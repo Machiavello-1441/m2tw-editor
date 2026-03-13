473b95c090772cb2d0b26dfdad3e3b4c34e0da46
Machiavello <machiavello1441@gmail.com>
2026-03-13 21:13:13 +0000

    File changes

--- src/components/ancillaries/AncillaryEditor.jsx
+++ src/components/ancillaries/AncillaryEditor.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAncillaries } from './AncillariesContext';
+
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
