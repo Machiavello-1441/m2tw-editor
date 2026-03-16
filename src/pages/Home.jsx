5eca57c54df09c24ef6d0fdbd0820a0642ae04be
Machiavello <machiavello1441@gmail.com>
2026-03-16 18:16:57 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -238,6 +238,7 @@ export default function Home() {
     const bldTgaFiles = [];
     const baseMapFiles = [];
     const groundTypeTgaFiles = [];
+    const stringsBinFiles = {};
 
     for (const file of files) {
       const name = file.name.toLowerCase();
