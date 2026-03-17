25526bb896d92ee7430ada10066e8371c33d75d8
Machiavello <machiavello1441@gmail.com>
2026-03-17 18:09:56 +0000

    File changes

---
+++ src/pages/ScriptEditor.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ScriptingPanel from '../components/map/ScriptingPanel';
+
+export default function ScriptEditor() {
+  return (
+    <div className="h-screen flex flex-col bg-slate-950">
+      <ScriptingPanel />
+    </div>
+  );
+}
