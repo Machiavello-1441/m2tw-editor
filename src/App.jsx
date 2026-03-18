a1931553973bc71b87c35ea3096c7058d97cce72
Machiavello <machiavello1441@gmail.com>
2026-03-18 08:17:54 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,6 @@ const AuthenticatedApp = () => {
       <Route path="/GoatTools" element={<LayoutWrapper currentPageName="GoatTools"><GoatTools /></LayoutWrapper>} />
       <Route path="/ScriptEditor" element={<LayoutWrapper currentPageName="ScriptEditor"><ScriptEditor /></LayoutWrapper>} />
       <Route path="/FactionsEditor" element={<LayoutWrapper currentPageName="FactionsEditor"><FactionsEditor /></LayoutWrapper>} />
-      <Route path="/CharactersEditor" element={<LayoutWrapper currentPageName="CharactersEditor"><CharactersEditor /></LayoutWrapper>} />
       <Route path="/CampaignSettings" element={<LayoutWrapper currentPageName="CampaignSettings"><CampaignSettings /></LayoutWrapper>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
