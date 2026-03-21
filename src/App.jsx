553223a104e190af3771f211145f56635e04c15d
Machiavello <machiavello1441@gmail.com>
2026-03-21 10:58:11 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -88,6 +88,7 @@ const AuthenticatedApp = () => {
       <Route path="/ScriptEditor" element={<LayoutWrapper currentPageName="ScriptEditor"><ScriptEditor /></LayoutWrapper>} />
       <Route path="/FactionsEditor" element={<LayoutWrapper currentPageName="FactionsEditor"><FactionsEditor /></LayoutWrapper>} />
       <Route path="/CampaignSettings" element={<LayoutWrapper currentPageName="CampaignSettings"><CampaignSettings /></LayoutWrapper>} />
+      <Route path="/MinorFiles" element={<LayoutWrapper currentPageName="MinorFiles"><MinorFiles /></LayoutWrapper>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
